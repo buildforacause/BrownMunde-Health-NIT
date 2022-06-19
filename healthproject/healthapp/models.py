e67617f9c70aebca8e6eaf3b2a8f9949a4463b55
Harmit Saini <92787535+buildforacause@users.noreply.github.com>
2022-06-19 16:43:29 +0530

    Add files via upload

--- healthproject/healthapp/models.py
+++ healthproject/healthapp/models.py
@@ -2,14 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-TYPE_CHOICES = (
-    ('Core', 'Core'),
-    ('Legs', 'Legs'),
-    ('Arms', 'Arms'),
-    ('Chest', 'Chest'),
-    ('Full-Body', 'Full-Body'),
-)
-
 
 class User(AbstractUser):
     pass
@@ -17,9 +9,9 @@ class User(AbstractUser):
 
 class Exercise(models.Model):
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=100, choices=TYPE_CHOICES, default='Full-Body')
+    type = models.CharField(max_length=100, default='')
     cal_bun = models.FloatField(default=0.0)
-    image = models.ImageField(upload_to="exercise/images", default="")
+    image = models.CharField(max_length=100000, default="")
     desc = models.TextField()
     link = models.CharField(max_length=2000, null=False)
 
